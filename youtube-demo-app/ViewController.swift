f8f9fbc09e8dadc1415dd1463f1baa23ef228d86
Adam Ludwin <aludwin@gmail.com>
2020-05-27 14:10:59 -0400

    initial xcode project creation clear
    ls

---
+++ youtube-demo-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-demo-app
+//
+//  Created by Adam Ludwin on 5/27/20.
+//  Copyright © 2020 Adam Ludwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
